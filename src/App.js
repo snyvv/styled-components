c7da9d183dad87c0124e01811351659c23a394d1
김신영/프론트개발팀 <ksy@musinsa.com>
2021-03-06 16:52:13 +0900

    Button Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="buttons">
+        <Button size="large" onClick={(e) => console.log('rest를 이용한 click 이벤트')}>BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="gray">BUTTON</Button>
+        <Button color="gray">BUTTON</Button>
+        <Button size="small" color="gray">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">BUTTON</Button>
+        <Button color="pink">BUTTON</Button>
+        <Button size="small" color="pink">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="large" fullWidth color="gray">
+          BUTTON
+        </Button>
+        <Button size="large" fullWidth color="pink">
+          BUTTON
+        </Button>
+      </div>
     </div>
   );
 }
